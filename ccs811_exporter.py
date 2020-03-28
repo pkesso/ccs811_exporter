4311dcbe3a8ed8289859b58f5d590e665659cda4
Paul K <pkesso@gmail.com>
2020-03-28 20:08:30 +0300

    --verbose option

--- ccs811_exporter.py
+++ ccs811_exporter.py
@@ -10,14 +10,17 @@ parser = argparse.ArgumentParser(description="Prometheus exporter for ccs811 air
 parser.add_argument('--listen', action='store', default='0.0.0.0', help='bind to address, default: 0.0.0.0')
 parser.add_argument('--port', action='store', type=int, default=8002, help='bind to port, default: 8002')
 parser.add_argument('--polling_interval', action='store', type=int, default=1, help='sensor polling interval, seconds, default: 1')
-# TODO verbose
+parser.add_argument('--verbose', action="store_true", help='print every poll result to stdout')
 
 args = parser.parse_args()
 
 ccs = Adafruit_CCS811()
 
 while not ccs.available():
+    if args.verbose:
+        print('ERROR ccs in not availabe during start')
     pass
+
 temp = ccs.calculateTemperature()
 ccs.tempOffset = temp - 25.0
 
@@ -33,13 +36,22 @@ def get_data():
         temp = ccs.calculateTemperature()
         if not ccs.readData():
             #print("CO2: ", ccs.geteCO2(), "ppm, TVOC: ", ccs.getTVOC(), " temp: ", temp)
-            temperature.set(ccs.calculateTemperature())
-            co2.set(ccs.geteCO2())
-            tvoc.set(ccs.getTVOC())
+            temperature_value=ccs.calculateTemperature()
+            co2_value=ccs.geteCO2()
+            tvoc_value=ccs.getTVOC()
+            temperature.set(temperature_value)
+            co2.set(co2_value)
+            tvoc.set(tvoc_value)
+            if args.verbose:
+                print("INFO temperature: ", temperature_value, " co2: ", co2_value, " tvoc: ", tvoc_value)
         else:
-            print(datetime.now(), " ERROR!")
+            print("ERROR ccs is not availale during poll")
             while(1):
                 pass
+    else:
+        print("ERROR cant read data from sensor")
+        while(1):
+            pass
     
 
 if __name__ == '__main__':
